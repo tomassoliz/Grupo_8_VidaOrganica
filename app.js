a7e9966f63f74211b9af68b44b922e910917770c
GasmauC <gaskk21@gmail.com>
2023-07-18 19:48:12 -0300

    uploading

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.get('/',(req,res) => res.sendFile(path.join(__dirname,'src/views','home.html
 app.get('/header',(req,res) => res.sendFile(path.join(__dirname,'src/partials','header.html'))) 
 app.get('/main',(req,res) => res.sendFile(path.join(__dirname,'src/partials','main.html'))) 
 app.get('/footer',(req,res) => res.sendFile(path.join(__dirname,'src/partials','footer.html'))) 
+app.get('/menu',(req,res) => res.sendFile(path.join(__dirname,'src/partials','menu.html'))) 
+
 
 
 app.listen(PORT, () => console.log(`Server running in http://localhost:${PORT}`));
