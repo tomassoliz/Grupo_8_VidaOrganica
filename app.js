047366a10baf20809d0a65dc858f8856b8ef36d2
Tomas Soliz <tomassoliz07@gmail.com>
2023-07-24 19:42:04 -0300

    arreglando cositas

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const PORT = 3030;
 app.use(express.static('public'))
 
 /*Rutas*/
-app.get('/', (req, res) => { res.sendFile(path.join(__dirname, "src", 'views', 'home.html')) });
-app.get('/register', (req, res) => { res.sendFile(path.join(__dirname, "src", "views", "register.html")) });
+app.get('/', (req, res) => { res.sendFile(path.join(__dirname, 'src', 'views', 'home.html')) });
+app.get('/register', (req, res) => { res.sendFile(path.join(__dirname, 'src', 'views', 'register.html')) });
 app.get('/header', (req, res) => { res.sendFile(path.join(__dirname, 'src/partials', 'header.html')) });
 app.get('/main', (req, res) => { res.sendFile(path.join(__dirname, 'src/partials', 'main.html')) });
 app.get('/footer', (req, res) => { res.sendFile(path.join(__dirname, 'src/partials', 'footer.html')) });
-app.get('/carrito', (req, res) => { res.sendFile(path.join(__dirname, "src","views","carrito.html")) });
+app.get('/carrito', (req, res) => { res.sendFile(path.join(__dirname, 'src','views','carrito.html')) });
 
 app.listen(PORT, () => console.log(`Server running in http://localhost:${PORT}`));
