7174f127f0f71dd467471114fe260f8d93999421
Tomas Soliz <tomassoliz07gmail.com>
2023-08-01 16:51:43 -0300

    empezando a corregir

--- app.js
+++ app.js
@@ -7,21 +7,14 @@ const PORT = 3030;
 app.use(express.static('public'))
 
 /*Rutas*/
-<<<<<<< HEAD
+
 app.get('/', (req, res) => { res.sendFile(path.join(__dirname, "src", 'views', 'home.html')) });
+app.get('/header', (req, res) => { res.sendFile(path.join(__dirname, 'src/partials', 'header.html')) });
+app.get('/main', (req, res) => { res.sendFile(path.join(__dirname, 'src/partials', 'main.html')) });
+app.get('/footer', (req, res) => { res.sendFile(path.join(__dirname, 'src/partials', 'footer.html')) });
 app.get('/register', (req, res) => { res.sendFile(path.join(__dirname, "src", "views", "register.html")) });
 app.get('/login', (req, res) => { res.sendFile(path.join(__dirname, "src", "views", "login.html")) });
-app.get('/header', (req, res) => { res.sendFile(path.join(__dirname, 'src/partials', 'header.html')) });
-app.get('/main', (req, res) => { res.sendFile(path.join(__dirname, 'src/partials', 'main.html')) });
-app.get('/footer', (req, res) => { res.sendFile(path.join(__dirname, 'src/partials', 'footer.html')) });
 app.get('/carrito', (req, res) => { res.sendFile(path.join(__dirname, "src", "views", "carrito.html")) });
-=======
-app.get('/', (req, res) => { res.sendFile(path.join(__dirname, 'src', 'views', 'home.html')) });
-app.get('/register', (req, res) => { res.sendFile(path.join(__dirname, 'src', 'views', 'register.html')) });
-app.get('/header', (req, res) => { res.sendFile(path.join(__dirname, 'src/partials', 'header.html')) });
-app.get('/main', (req, res) => { res.sendFile(path.join(__dirname, 'src/partials', 'main.html')) });
-app.get('/footer', (req, res) => { res.sendFile(path.join(__dirname, 'src/partials', 'footer.html')) });
-app.get('/carrito', (req, res) => { res.sendFile(path.join(__dirname, 'src','views','carrito.html')) });
->>>>>>> develop
+
 
 app.listen(PORT, () => console.log(`Server running in http://localhost:${PORT}`));
