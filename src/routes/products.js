04693d10b0d103d112674bb377b6421a1dabbad0
Tomas Soliz <tomassoliz07@gmail.com>
2023-08-10 19:17:34 -0300

    cambiando rutas /productsDetails

--- src/routes/products.js
+++ src/routes/products.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const {products,carrito} = require('../controllers/productsController')
 
+/* /products */
+
 router.get('/productsDetails',products)
 router.get('/carrito', carrito)
 
