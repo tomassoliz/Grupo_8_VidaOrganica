552ccc344833ddcb8f2cf58abd5e95da5f82ede7
Tomas Soliz <tomassoliz07@gmail.com>
2023-08-11 01:23:11 -0300

    agregando archivo /productsAdd

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const {products,carrito} = require('../controllers/productsController')
+const { products, carrito, add } = require('../controllers/productsController')
 
 /* /products */
 
-router.get('/productsDetails',products)
+router.get('/productsDetails', products)
 router.get('/carrito', carrito)
+router.get('/productsAdd', add)
 
 module.exports = router;
