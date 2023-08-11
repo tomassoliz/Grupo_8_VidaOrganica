d6bb9739ea915af3f90a80fdc7d65151b567f1d1
Ezequiel Paredes <ezemaidana44@gmail.com>
2023-08-11 04:37:58 -0300

    Wip:vista_editar-producto

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const { products, carrito, add ,edit} = require('../controllers/productsController')
+const { products, carrito, add, edit } = require('../controllers/productsController')
 
 /* /products */
 
 router.get('/productsDetails', products)
 router.get('/carrito', carrito)
+router.get('/productsAdd', add)
+router.get('/productEdit', edit)
 
 module.exports = router;
