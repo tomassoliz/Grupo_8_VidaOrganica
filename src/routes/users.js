ce7462390623604382c9c02e6c89660dc2b67de2
Tomas Soliz <tomassoliz07@gmail.com>
2023-08-11 21:25:57 -0300

    errores de clases

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,11 @@
-const  express = require('express');
-const  router = express.Router();
-const {register,login} = require('../controllers/usersController')
+const express = require('express');
+const router = express.Router();
+const { register, login } = require('../controllers/usersController')
 
 /* /users */
 
-router.get('/register',register);
-router.get('/login',login);
- 
+router.get('/register', register);
+router.get('/login', login);
+
 
 module.exports = router;
