17d862cd59c78d49d119bcb5d26548b9ca3c687d
Tomas Soliz <tomassoliz07gmail.com>
2023-08-24 11:52:20 -0300

    pull develop

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,4 +1,4 @@
-const { v4: uuidv4 } = require("uuid");
+// const { v4: uuidv4 } = require("uuid");
 const { unlinkSync, existsSync } = require("fs");
 const { readJSON, writeJSON } = require("../controllers/data/index");
 
