7774bfdc70b7af1e187c15f1b5639535639d8346
Lula <ludmilamiceli@hotmail.com>
2023-08-26 21:14:58 -0300

    Fix image

--- src/data/product.js
+++ src/data/product.js
@@ -1,6 +1,6 @@
 const {v4 : uuidv4} = require('uuid');
 
-const Product = function ({name,brand,price,discount,description}) {
+const Product = function ({name,brand,price,discount,description, image}) {
     this.id = uuidv4();
     this.name = name.trim();
     this.brand = brand;
@@ -8,6 +8,7 @@ const Product = function ({name,brand,price,discount,description}) {
     this.discount = +discount;
     this.description = description.trim()
     this.createdAT = new Date();
+    this.image = image
 };
 
 module.exports = Product;
