2c50e922aa1c8d2134f29fb0939c603470c6f4ec
Tomas Soliz <tomassoliz07@gmail.com>
2023-09-02 16:48:39 -0300

    cambios de diseño

--- src/data/product.js
+++ src/data/product.js
@@ -1,15 +1,13 @@
-const { v4: uuidv4 } = require('uuid');
+const {v4 : uuidv4} = require('uuid');
 
-const Product = function ({name,brand,price,discount,description, image}) {
+const Product = function ({name,brand,price,discount,description}) {
     this.id = uuidv4();
     this.name = name.trim();
     this.brand = brand;
     this.price = +price;
-    this.category = category
     this.discount = +discount;
     this.description = description.trim()
     this.createdAT = new Date();
-    this.image = image
 };
 
 module.exports = Product;
