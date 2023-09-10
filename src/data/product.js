f4477af063f6b39e5217fb364156b572f4ef5496
Lula <ludmilamiceli@hotmail.com>
2023-09-10 20:38:25 -0300

    Product

--- src/data/product.js
+++ src/data/product.js
@@ -1,13 +1,12 @@
 const {v4 : uuidv4} = require('uuid');
 
-const Product = function ({name,brand,price,discount,description, category, image}) {
+const Product = function ({name,brand,price,discount,description}) {
     this.id = uuidv4();
     this.name = name.trim();
     this.brand = brand;
     this.price = +price;
     this.discount = +discount;
-    this.description = description.trim();
-    this.category = category;
+    this.description = description.trim()
     this.createdAT = new Date();
     this.image = image
 };
