9989af873875da709e1570652f42e4dbe8af2fb8
GasmauC <gaskk21@gmail.com>
2023-09-18 21:27:14 -0300

    upload:login validator

--- src/validations/loginValidator.js
+++ src/validations/loginValidator.js
@@ -8,11 +8,11 @@ module.exports = [
     .isEmail().withMessage('El Fomrato no es correcto!'),
 
     body('password')
-        .notEmpty('').withMessage('La contraseña es requreida!!').bail()
+        .notEmpty().withMessage('La contraseña es requreida!!').bail()
         .custom((value,{req})=>{
             const users = readJSON('users.json')
             const user= users.find(user => user.email === req.body.email)// primero validamos que sea el usuario para despues validar el pass
-            if(!user|| !comparesSync(value,user.password)){ // verificamos si existe el usuario!
+            if(!user|| !compareSync(value,user.password)){ // verificamos si existe el usuario!
                 return false
 
             }
