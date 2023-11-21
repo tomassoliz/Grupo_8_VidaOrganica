fce413ab853d24f00bb857fd34a1c995623d205d
GasmauC <gaskk21@gmail.com>
2023-11-21 19:14:59 -0300

    sprint 7

---
+++ public/javascripts/loginValidation.js
@@ -0,0 +1,98 @@
+const  $ = id => document.getElementById(id)
+
+window.onload = function(){ 
+    
+    $('email').addEventListener('focus', function(e){
+    $('msgError-Email').innerHTML = null
+    this.classList.remove('is-invalid');
+    this.classList.remove('is-valid')
+})
+
+$('email').addEventListener('blur', function(e){
+
+    switch (true) {
+        case !this.value.trim():
+            $('msgError-Email').innerHTML = "El email es obligatorioooooo!!!"
+            this.classList.add('is-invalid')
+            break;
+        case !/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(this.value.trim()):
+            $('msgError-Email').innerHTML = "El formato es inválidoooooooo";
+            this.classList.add('is-invalid')
+            break
+        default:
+            $('msgError-Email').innerHTML = null;
+            this.classList.add('is-valid')
+            this.classList.remove('is-invalid')
+            break;
+    }
+});
+
+$('email').addEventListener('change', async function(e){
+
+    try {
+
+        const response = await fetch(`/apis/check-email?email=${this.value.trim()}`)
+        const result = await response.json()
+
+        if(!result.data) {
+            $('msgError-Email').innerHTML = "El email es Incorrecto"
+            this.classList.add('is-invalid')
+        }
+        
+        
+    } catch (error) {
+        console.error(error);
+    }
+})
+
+$('password').addEventListener('focus', function(e){
+    $('msgError-Pass').innerHTML = null
+    this.classList.remove('is-invalid');
+    this.classList.remove('is-valid')
+})
+
+$('password').addEventListener('blur', function(e){
+
+    switch (true) {
+        case !this.value.trim():
+            $('msgError-Pass').innerHTML = "La contraseña es obligatoriaaaaaaaa MILEI PRESIDENTE!!!"
+            this.classList.add('is-invalid')
+            break;
+        case !/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{6,12}$/.test(this.value.trim()):
+            $('msgError-Pass').innerHTML = "La contraseña es incorrecta!!!";
+            this.classList.add('is-invalid')
+            break
+        default:
+            $('msgError-Pass').innerHTML = null;
+            this.classList.add('is-valid')
+            this.classList.remove('is-invalid')
+            break;
+    }
+});
+
+$('viewPassword').addEventListener('click', function(e) {
+    
+    $('msgError-Pass').innerHTML = null
+    $('password').classList.remove('is-invalid');
+    $('password').classList.remove('is-valid');
+
+    $('password').type = $('password').type === "text" ? "password" : "text"
+
+    this.classList.toggle("fa");
+    this.classList.toggle("fa-eye");
+
+    this.classList.toggle("fa-solid");
+    this.classList.toggle("fa-eye-slash");
+
+});
+
+
+$('password').addEventListener('focus', function(e){
+    $('msgError-Pass').innerHTML = null
+    this.classList.remove('is-invalid');
+    this.classList.remove('is-valid')
+})
+
+
+
+}
---
+++ public/javascripts/profileValidator.js
@@ -0,0 +1,125 @@
+const $ = id => document.getElementById(id);
+
+window.onload = async function(e){
+
+    $('name').addEventListener('blur', function(e){
+
+        switch (true) {
+            case !this.value.trim():
+                $('msgError-name').innerHTML = "El nombre es obligatorio"
+                this.classList.add('is-invalid')
+                break;
+            case this.value.trim().length < 2:
+                $('msgError-name').innerHTML = "Mínimo dos letras";
+                this.classList.add('is-invalid')
+                break
+            case !/^[ a-zA-ZñÑáéíóúÁÉÍÓÚ]+$/.test(this.value.trim()):
+                $('msgError-name').innerHTML = "Solo se permiten letras";
+                this.classList.add('is-invalid')
+                break
+            default:
+                $('msgError-name').innerHTML = null;
+                this.classList.add('is-valid')
+                this.classList.remove('is-invalid')
+                break;
+        }
+    });
+
+    
+
+    $('surname').addEventListener('blur', function(e){
+
+        switch (true) {
+            case !this.value.trim():
+                $('msgError-surname').innerHTML = "El apellido es obligatorio"
+                this.classList.add('is-invalid')
+                break;
+            case this.value.trim().length < 2:
+                $('msgError-surname').innerHTML = "Mínimo dos letras";
+                this.classList.add('is-invalid')
+                break
+            case !/^[ a-zA-ZñÑáéíóúÁÉÍÓÚ]+$/.test(this.value.trim()):
+                $('msgError-surname').innerHTML = "Solo se permiten letras";
+                this.classList.add('is-invalid')
+                break
+            default:
+                $('msgError-surname').innerHTML = null;
+                this.classList.add('is-valid')
+                this.classList.remove('is-invalid')
+                break;
+        }
+    });
+
+    $('email').addEventListener('focus', function(e){
+        $('msgError-email').innerHTML = null
+        this.classList.remove('is-invalid');
+        this.classList.remove('is-valid')
+    })
+    
+    $('email').addEventListener('blur', function(e){
+    
+        switch (true) {
+            case !this.value.trim():
+                $('msgError-email').innerHTML = "El email es obligatorioo!!!"
+                this.classList.add('is-invalid')
+                break;
+            case !/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(this.value.trim()):
+                $('msgError-email').innerHTML = "El formato es inválidoooooooo";
+                this.classList.add('is-invalid')
+                break
+            default:
+                $('msgError-email').innerHTML = null;
+                this.classList.add('is-valid')
+                this.classList.remove('is-invalid')
+                break;
+        }
+    });
+    
+    $('email').addEventListener('blur', function(e){
+
+        switch (true) {
+            case !this.value.trim():
+                $('msgError-email').innerHTML = "El email es obligatoriooooooo MILEI PRESIDENTE!!!"
+                this.classList.add('is-invalid')
+                break;
+            case !/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(this.value.trim()):
+                $('msgError-email').innerHTML = "El formato es inválidoooooooo";
+                this.classList.add('is-invalid')
+                break
+            default:
+                $('msgError-email').innerHTML = null;
+                this.classList.add('is-valid')
+                this.classList.remove('is-invalid')
+                break;
+        }
+    });
+
+    
+
+    $('birthday').addEventListener('blur', function(e){
+        const birthday = moment(this.value);
+        const minDate = moment().subtract(100,'years');
+        const currentDate = moment();
+
+        switch (true) {
+
+            case birthday.isBefore(minDate):
+                $('msgError-birthday').innerHTML = " Al cementerio..";
+                this.classList.add('is-invalid')
+                break
+            case birthday.isAfter(currentDate):
+                $('msgError-birthday').innerHTML = "ummm??";
+                this.classList.add('is-invalid')
+                break
+            default:
+                $('msgError-birthday').innerHTML = null;
+                this.classList.add('is-valid')
+                this.classList.remove('is-invalid')
+                break;
+        }
+    });
+
+
+  
+
+}
