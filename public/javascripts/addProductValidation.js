a09deae6272e85bc48f010e427f0a835e7aa340a
Tomas Soliz <tomassoliz07@gmail.com>
2023-11-24 21:00:46 -0300

    solucionando problemas

--- public/javascripts/addProductValidation.js
+++ public/javascripts/addProductValidation.js
@@ -1,6 +1,103 @@
-const form = document.getElementById('productAdd');/* captura el formulario con id = productAdd */
+const form = document.getElementById('formulario');/* captura el formulario con id = productAdd */
+const inputs = document.querySelectorAll('#formulario input'); /* accede a los inputs del form con id = productAdd */
 
-const inputs = document.querySelectorAll('#productAdd input'); /* accede a los inputs del form con id = productAdd */
+
+const expresiones = {
+	nombre: /^[a-zA-ZÀ-ÿ\s]{5,40}$/, // Letras y espacios, pueden llevar acentos.
+	precio: /^\d{2,15}$/, // 2 a 5 numeros.
+	descripcion: /^[a-zA-ZÀ-ÿ\s\d]{20,500}$/ // Descripcion,
+}
+
+const campos = {
+	name: false,
+	price: false,
+	description: false
+}
+
+const validarForm = (e) => {
+   switch (e.target.name) {
+    case 'name':
+        validarCampo(expresiones.nombre, e.target, 'name');
+    break;
+    case 'price':
+        validarCampo(expresiones.precio, e.target, 'price');
+    break;
+
+    case 'description':
+        validarCampo(expresiones.descripcion, e.target, 'description');
+    break;
+   }
+}
+
+const validarCampo = (expresion, input, campo) => {
+	if (expresion.test(input.value)) {
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+		campos[campo] = true;
+	} else {
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+		campos[campo] = false;
+	}
+}
+
+inputs.forEach((input) =>{
+    input.addEventListener('keyup', validarForm);
+    input.addEventListener('blur', validarForm);
+})
+
+
+
+
+form.addEventListener('submit', () =>{
+    // e.preventDefault();
+
+    if (campos.name && campos.price && campos.description) {
+        // form.reset();
+
+        document.getElementById('form_mensaje-exito').classList.add('form_mensaje-exito-activo');
+        setTimeout(() => {
+			document.getElementById('form_mensaje-exito').classList.remove('form_mensaje-exito-activo');
+		}, 5000);
+
+		document.querySelectorAll('.form__grupo-correcto').forEach((icono) => {
+			icono.classList.remove('form__grupo-correcto');
+		});
+	} else {
+		document.getElementById('form_mensaje').classList.add('form_mensaje-activo');
+	}
+    
+}); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* const form = document.getElementById('productAdd');/* captura el formulario con id = productAdd 
+
+const inputs = document.querySelectorAll('#productAdd input'); // accede a los inputs del form con id = productAdd 
 
 
 const expresiones = {
@@ -13,7 +110,7 @@ const expresiones = {
 const validarForm = (e) => {
    switch (e.target.name) {
     case 'name':
-        validarCampo(expresiones.name, e.target, 'name');
+        validarCampo(expresiones.nombre, e.target, 'name');
     break;
     case 'price':
         validarCampo(expresiones.precio, e.target, 'price');
@@ -51,32 +148,7 @@ inputs.forEach((input) =>{
 
 form.addEventListener('submit', (e) =>{
     e.preventDefault();
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}); */
 
 /* BASADO EN PG */
 
--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -7,22 +7,22 @@ const productsFile = path.join(__dirname, '../data/products.json');
 const products = JSON.parse(fs.readFileSync(productsFile, 'utf-8'));
 
 const db = require('../database/models');
-const {Op} = require('sequelize')
+const { Op } = require('sequelize')
 
 module.exports = {
     index: async (req, res) => {
 
         try {
-            const carousell = readJSON('carousell.json'); 
+            const carousell = readJSON('carousell.json');
             const products = await db.Image.findAll({
-                include : [
-                    { 
+                include: [
+                    {
                         model: db.Product
                     }
-                ]    
+                ]
             })
-            
-            return res.render('index',{
+
+            return res.render('index', {
                 products,
                 carousell
             })
@@ -38,22 +38,22 @@ module.exports = {
             const products = await db.Image.findAll({ //uso primero el modelo de images dso hago los incluide tmb utiliando lo que tiene produt como asocion
                 include: [
                     {
-                      model: db.Product,
-                      include : [ 
-                        {
-                            model: db.Brand, 
-                            // attributes : ['id', 'name'] para pasar solamente lo que necesite el front
-                        },
-                        {
-                            model: db.Section
-                        },
-                        {
-                            model: db.Category
-                        }
-                      ]
+                        model: db.Product,
+                        include: [
+                            {
+                                model: db.Brand,
+                                // attributes : ['id', 'name'] para pasar solamente lo que necesite el front
+                            },
+                            {
+                                model: db.Section
+                            },
+                            {
+                                model: db.Category
+                            }
+                        ]
                     }
-                  ]
-            }) 
+                ]
+            })
             const brands = await db.Brand.findAll({
                 order: ['name']
             })
@@ -63,55 +63,59 @@ module.exports = {
             const sections = await db.Section.findAll({
                 order: ['name']
             })
-            const adminUser = await db.User.findAll()                    
-                    return res.render('admin', {
-                        carousell,
-                        products,
-                        brands,
-                        sections,
-                        categories,
-                        adminUser
-                    })
+            const adminUser = await db.User.findAll()
+            return res.render('admin', {
+                carousell,
+                products,
+                brands,
+                sections,
+                categories,
+                adminUser
+            })
 
         } catch (err) {
             console.log("Error Product create route: ", err);
         }
 
     },
-    search: (req, res) => {
-        /* const products = readJSON('products.json'); */
+    search: async (req, res) => {
 
-        const keywords = req.query.keywords
-        /* const results = products.filter(product => product.name.toLowerCase().includes(keywords.toLowerCase()))
-
-        return res.render('results', {
-            products,
-            keywords,
-            results
-        }) */
-        db.Product.findAll({
-            where: {
-                [Op.or] : [
+        try {
+            const keywords = req.query.keywords
+            const products = await db.Image.findAll({
+                include: [
                     {
-                        name: {
-                            [Op.substring] : keywords
-                        }
-                    },
-                    {
-                        description: {
-                            [Op.substring] : keywords
-                        }
-                    }
-                ]
+                        model: db.Product,
+                        where: {
+                            [Op.or]: [
+                                {
+                                    name: {
+                                        [Op.substring]: keywords
+                                    }
+                                },
+                                {
+                                    description: {
+                                        [Op.substring]: keywords
+                                    }
+                                }
+                            ]
                 
-            }
-        })
-            .then(results => {
-                return res.render('results',{
-                    results,
-                    keywords
-            })
-        }).catch(error => console.log(error))
-    }
+                        } 
+                    }
+                    
+                ]
+            })  
+            // const product = await db.Product.findAll({
+                
+            // })
 
+            return res.render('results', {
+                products,
+                // product,
+                keywords   
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }
 }
